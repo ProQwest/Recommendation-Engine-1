d9c3f83a5d7abd15c0c802d563fa6e3e9764d8c0
Prashanth Ramanna <prashant.ramanna@gmail.com>
2013-08-24 16:13:12 -0700

    Added Prediction of ratings for profiles based on memory model.

--- RecommendationSystem/src/com/recommendation/engine/Orchaestrator.scala
+++ RecommendationSystem/src/com/recommendation/engine/Orchaestrator.scala
@@ -1,28 +1,56 @@
 package com.recommendation.engine
 
-import com.recommendation.profile.Profile
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.FileInputStream
 import scala.collection.mutable.HashMap
-import scala.util.Try
-import com.recommendation.utils.logger
+
 import com.recommendation.profile.Profile
-import java.io.FileNotFoundException
 import com.recommendation.similarity.Similarity
+import com.recommendation.utils.logger
 
 object Orchaestrator extends logger {
-  
-  
-  
-  def main(args : Array[String]){
-    val profileMap =Profiler.setUpProfiles(args(0))
+
+  def main(args: Array[String]) {
+    val profileMap = Profiler.setUpProfiles(args(0))
+    calculateSimilarity(profileMap)
+
     
-    profileMap foreach {entryX =>
-      profileMap foreach {entryY =>
-        if(entryY!= entryX)Similarity.pearson(entryX._2, entryY._2)
+    profileMap foreach { entryX =>
+      var normFactor = 0.0
+      profileMap foreach { entryY =>
+        if (entryY != entryX) {
+          val sim = entryX._2.similarityMap.get(entryY._1).get
+          normFactor += sim.abs
+          entryY._2.ratingMap foreach { ratedItem =>
+            if (!entryX._2.ratingMap.contains(ratedItem._1)) {
+              entryX._2.predictedRatingMap.get(ratedItem._1) match {
+                case Some(predictedItem) =>
+                  val predict = sim * (ratedItem._2 - entryY._2.avgRating)
+                  entryX._2.predictedRatingMap.put(ratedItem._1, predictedItem + predict)
+
+                case None =>
+                  val predict = sim * (ratedItem._2 - entryY._2.avgRating)
+                  entryX._2.predictedRatingMap.put(ratedItem._1, predict)
+              }
+            }
+          }
+        }
       }
-      
+      normFactor = 1/normFactor
+      entryX._2.predictedRatingMap = entryX._2.predictedRatingMap map { predicted =>
+         (predicted._1,entryX._2.avgRating + normFactor * predicted._2)
+      }
+    }
+  }
+
+  /**
+   * Calculates each profiles similarity w.r.t others
+   * TODO : can leverage the double loop for reuse
+   */
+  private def calculateSimilarity(profileMap: HashMap[String, Profile]): Unit = {
+
+    profileMap foreach { entryX =>
+      profileMap foreach { entryY =>
+        if (entryY != entryX) Similarity.pearson(entryX._2, entryY._2)
+      }
     }
   }
   
